2242224bb85b6d3caa48553fea46b8297c819b69
Keane <KeaneCarotenuto@gmail.com>
2020-07-17 15:32:15 +1200

    Basic Player Movement
    Use W to move in look direction, Shift to Sprint

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isPossesed = false;
+    public GameObject playerCamera;
+
+    public float walkForce = 100;
+    public float maxWalkSpeed = 10;
+    public float maxSprintSpeed = 30;
+    
+    [Range(0.0f, 1.0f)] public float drag;
+
+    float maxSpeed;
+
+   void Start()
     {
-        
+        drag = 1 - drag;
+        maxSpeed = maxWalkSpeed;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //drag
+        GetComponent<Rigidbody>().velocity = new Vector3(drag * GetComponent<Rigidbody>().velocity.x, GetComponent<Rigidbody>().velocity.y, drag * GetComponent<Rigidbody>().velocity.z);
+
+        //Sprint Controller
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            maxSpeed = maxSprintSpeed;
+        }
+        else
+        {
+            maxSpeed = maxWalkSpeed;
+        }
+
+        //Forward Movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            //Smooths out movement
+            GetComponent<Rigidbody>().AddForce(walkForce * transform.forward * (1 - GetComponent<Rigidbody>().velocity.magnitude / maxSpeed));
+        }
         
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //    GetComponent<Rigidbody>().velocity += transform.forward * (1 - GetComponent<Rigidbody>().velocity.magnitude/maxSpeed);
+        //}
+
+        //Controls Horizontal rotation of Player + Player Cam
+        float newRotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * 3;
+        transform.localEulerAngles = new Vector3(0f, newRotationX, 0f);
     }
 }
---
+++ Assets/PlayerCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public GameObject character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Control Vertical rotaiton seperate from horizontal.
+        float newRotationY = transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * 3;
+        transform.localEulerAngles = new Vector3(newRotationY, 0f, 0f);
+    }
+}
