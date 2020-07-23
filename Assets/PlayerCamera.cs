b79dcd52cdafeda234ef6946628318fdb9e85a9c
Keane <keanecarotenuto@gmail.com>
2020-07-24 09:47:16 +1200

    Removed Old Code

--- Assets/PlayerCamera.cs
+++ Assets/PlayerCamera.cs
@@ -10,16 +10,12 @@ public class PlayerCamera : MonoBehaviour
 
     float xRotation = 0f;
 
-    
-
-    // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
