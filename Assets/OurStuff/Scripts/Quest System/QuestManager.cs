e252c5f3f052cc8f7332b1276675cf0df6ed2beb
Nerys <NerysThamm@gmail.com>
2020-07-31 15:15:41 +1200

    Added quest system
    Added Quest and questObjective scriptableobjects, added QuestManager script, added QuestManager gameobject, added QuestManager tag, created test objective, created test quest.

---
+++ Assets/OurStuff/Scripts/Quest System/QuestManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class QuestEvent : UnityEvent<string, ObjectiveType> { }
+public class QuestManager : MonoBehaviour
+{
+    [SerializeField]
+    public QuestEvent OnPlayerAction;
+    [SerializeField]
+    private List<Quest> activeQuests;
+    [SerializeField]
+    private List<Quest> completedQuests;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnPlayerAction.AddListener(CheckQuestStatus);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CheckQuestStatus(string _targetID, ObjectiveType _type)
+    {
+        foreach(Quest quest in activeQuests)
+        {
+            quest.checkObjective(_targetID, _type);
+            if(quest.questComplete)
+            {
+                completedQuests.Add(quest);
+                activeQuests.Remove(quest);
+            }
+        }
+    }
+
+    public void TryAddQuest(Quest _quest)
+    {
+        if(activeQuests.Contains(_quest) || completedQuests.Contains(_quest))
+        {
+            return;
+        }
+        else
+        {
+            activeQuests.Add(_quest);
+        }
+    }
+}
