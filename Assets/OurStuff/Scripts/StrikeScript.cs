1170d0a6cff76576760b8436d5e21acea5570bc2
Keane <keanecarotenuto@gmail.com>
2020-08-07 10:54:10 +1200

    Started Working on Bolt + made UI Scale

---
+++ Assets/OurStuff/Scripts/StrikeScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrikeScript : MonoBehaviour
+{
+    public float startHeight;
+    public float turnAmount;
+    public float turnHorizDistance;
+    public bool randomVertSpacing;
+    public float Speed;
+
+    public GameObject boltToUse;
+    public GameObject target;
+
+    private List<Vector3> nodes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nodes.Add(target.transform.position + Vector3.up * startHeight);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
