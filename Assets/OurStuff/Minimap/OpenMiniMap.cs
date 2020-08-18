e9d01a0baa36ba7a5a4f24a3087c2d5a014c30aa
sjafetsalgo <sjafetsalgo15@gmail.com>
2020-08-18 22:23:56 +1200

    Reorganizing
    Moved a few stuffs around and deleted empty folders

---
+++ Assets/OurStuff/Minimap/OpenMiniMap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMiniMap : MonoBehaviour
+{
+    public bool MiniMapOpen = false;
+    public GameObject BigMiniMap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M) && MiniMapOpen == false)
+        {
+            BigMiniMap.SetActive(true);
+            MiniMapOpen = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.M) && MiniMapOpen == true)
+        {
+            BigMiniMap.SetActive(false);
+            MiniMapOpen = false;
+        }
+    }
+}
---
+++ Assets/OurStuff/Scripts/DialogueSystem/Textbox/Dialogue/SampleDialogue/DrawerInspectDialogue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerInspectDialogue : MonoBehaviour
+{
+    public TextBoxController TBcontroller;
+    void Start()
+    {
+        TBcontroller = GameObject.FindWithTag("TextBoxService").GetComponent<TextBoxController>();
+    }
+    [SerializeField]
+    public List<Sprite> CharSprites = new List<Sprite>();
+    // Start is called before the first frame update
+    public void Initiate()
+    {
+        
+
+        TBcontroller.CreateTextBox("...", CharSprites[1]);
+        TBcontroller.CreateTextBox("Whats this doing here?", CharSprites[2]);
+        TBcontroller.NextTextBox();
+        
+
+
+    }
+}
