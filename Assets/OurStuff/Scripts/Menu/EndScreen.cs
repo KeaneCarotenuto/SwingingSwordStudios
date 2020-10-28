1f358533c2f8f26642147e14a69b137b32049dc8
Steven Tan <tan2001steven@hotmail.com>
2020-10-28 23:12:27 +1300

    Added End Screen

---
+++ Assets/OurStuff/Scripts/Menu/EndScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScreen : MonoBehaviour
+{
+    public GameObject title;
+    public GameObject button;
+    public GameObject button2;
+    public float delay = 8;
+    // Start is called before the first frame update
+    void Start()
+    {
+        title.SetActive(false);
+        button.SetActive(false);
+        button2.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(SetActive());
+    }
+
+    public IEnumerator SetActive()
+    {
+        yield return new WaitForSeconds(delay);
+        title.SetActive(true);
+        button.SetActive(true);
+        button2.SetActive(true);
+    }
+
+}
