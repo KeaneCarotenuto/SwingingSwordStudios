011471468d8cfa87c6b710c6f61a19ba734e1ee2
Keane <KeaneCarotenuto@gmail.com>
2020-10-30 22:43:49 +1300

    Com

--- Assets/QuestWall.cs
+++ Assets/QuestWall.cs
@@ -1,17 +1,35 @@
-﻿using System.Collections;
+﻿////////////////////////////////////////////////////////////
+//========================================================//
+// Bachelor of Software Engineering                       //
+// Media Design School                                    //
+// Auckland                                               //
+// New Zealand                                            //
+//--------------------------------------------------------//
+// (c) 2020 Media Design School                           //
+//========================================================//
+//   File Name  : QuestWall.cs
+//--------------------------------------------------------//
+//  Description : 
+//  Manages a giant collider wall that stops player progression if they havent completed a quest,
+//  And then removes itself if the quest has been completed.
+//  
+//--------------------------------------------------------//
+//    Author    : Keane Carotenuto BSE20021               //
+//--------------------------------------------------------//
+//    E-mail    : KeaneCarotenuto@gmail.com               //
+//========================================================//
+////////////////////////////////////////////////////////////
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class QuestWall : MonoBehaviour
 {
+    //The quest that needs to be done
     public Quest questToUnlock;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    //If quest done, del self
     void Update()
     {
         if (questToUnlock.questComplete)
