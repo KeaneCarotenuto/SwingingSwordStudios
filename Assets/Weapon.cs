308924eb51aad3eec168e2dabb8df3111d937185
Keane <KeaneCarotenuto@gmail.com>
2020-10-30 22:50:03 +1300

    Com

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -1,16 +1,33 @@
-﻿using System.Collections;
+﻿////////////////////////////////////////////////////////////
+//========================================================//
+// Bachelor of Software Engineering                       //
+// Media Design School                                    //
+// Auckland                                               //
+// New Zealand                                            //
+//--------------------------------------------------------//
+// (c) 2020 Media Design School                           //
+//========================================================//
+//   File Name  : Wepaons.cs
+//--------------------------------------------------------//
+//  Description : 
+//  This with this script deal damage to the player on contact
+//  
+//  
+//--------------------------------------------------------//
+//    Author    : Keane Carotenuto BSE20021               //
+//--------------------------------------------------------//
+//    E-mail    : KeaneCarotenuto@gmail.com               //
+//========================================================//
+////////////////////////////////////////////////////////////
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    //If touches player, deal damage
     private void OnTriggerEnter(Collider other)
     {
         if (other.name.Contains("Player"))
