f3b878eb6c9ae275abacc6e15f7baeb16cdfefae
Keane <KeaneCarotenuto@gmail.com>
2020-10-30 22:38:10 +1300

    Com

--- Assets/OurStuff/Scripts/Player/PlayerCamera.cs
+++ Assets/OurStuff/Scripts/Player/PlayerCamera.cs
@@ -1,4 +1,26 @@
-﻿using System.Collections;
+﻿////////////////////////////////////////////////////////////
+//========================================================//
+// Bachelor of Software Engineering                       //
+// Media Design School                                    //
+// Auckland                                               //
+// New Zealand                                            //
+//--------------------------------------------------------//
+// (c) 2020 Media Design School                           //
+//========================================================//
+//   File Name  : PlayerCamera.cs
+//--------------------------------------------------------//
+//  Description : 
+//  Manages how the camera moves
+//  
+//  
+//--------------------------------------------------------//
+//    Author    : Keane Carotenuto BSE20021               //
+//--------------------------------------------------------//
+//    E-mail    : KeaneCarotenuto@gmail.com               //
+//========================================================//
+////////////////////////////////////////////////////////////
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,16 +36,19 @@ public class PlayerCamera : MonoBehaviour
 
     void Start()
     {
+        //Disable cursor
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
+        //Change sensitivity depending on if in dialogue
         mouseSensitivity = (tbcontroller.InDialogue ? 10f : 100f);
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        //Rotate camera with mouse
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
